d490487ee26bb54d0526b0f89acbd84590f5d347
ggorsontanguy <guillaume@gorson-tanguy.net>
2015-12-29 22:03:25 +0100

    Default state of classes

--- src/test/java/net/kata/AppTest.java
+++ src/test/java/net/kata/AppTest.java
@@ -1,15 +1,32 @@
 package net.kata;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class AppTest {
 
-	@Test
-	public final void test() {
-		fail("Not yet implemented"); // TODO
+	private App app;
+	
+	@Before
+	public void before() {
+		app = new App();
 	}
+	
 
+	@Test
+	public void shouldReturn() {
+		// GIVEN
+		
+		// WHEN
+		String actual = "1";
+		
+		String expected = "1";
+		
+		// THEN
+		assertThat(actual).isEqualTo(expected);
+	}
+	
 }
