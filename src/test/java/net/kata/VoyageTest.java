624130948866e51e070117385a8fe3795d988372
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-04 21:09:17 +0100

    TDD RED should_have_two_town_not_equals

--- src/test/java/net/kata/VoyageTest.java
+++ src/test/java/net/kata/VoyageTest.java
@@ -17,5 +17,17 @@ public class VoyageTest {
 		assertThat(actual).isNotEmpty();
 	}
 	
+	@Test
+	public void should_have_two_town_not_equals() {
+		// GIVEN
+		Town given_1 = new Town();
+		Town given_2 = new Town();
+		
+		// WHEN
+		boolean actual = given_1.getName().equals(given_2.getName());
+		
+		// THEN
+		assertThat(actual).isFalse();
+	}
 	
 }
