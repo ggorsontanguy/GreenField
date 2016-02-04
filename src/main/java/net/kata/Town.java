00e0a663c7b588b9d6a2ccc4070148d2ca3d1825
Guillaume Gorson-Tanguy <guillaume@gorson-tanguy.net>
2016-02-04 21:15:51 +0100

    TDD GREEN + should_have_same_town_be_equals_itself

--- src/main/java/net/kata/Town.java
+++ src/main/java/net/kata/Town.java
@@ -1,9 +1,21 @@
 package net.kata;
 
+import net.kata.Town.Name;
+
 public class Town {
 
+	enum Name {
+		PARIS, LYON
+	}
+	
+	Name name;
+	
+	public Town(Name name) {
+		this.name = name;
+	}
+
 	public String getName() {
-		return "PARIS";
+		return name.name();
 	}
 
 }
--- src/test/java/net/kata/VoyageTest.java
+++ src/test/java/net/kata/VoyageTest.java
@@ -1,14 +1,18 @@
 package net.kata;
 
 import org.junit.Test;
+
+import static net.kata.Town.Name.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class VoyageTest {
 
 	@Test
 	public void name_should_be_never_empty_given_any_town() {
 		// GIVEN
-		Town given = new Town();
+		Town given = new Town(PARIS);
 		
 		// WHEN
 		String actual = given.getName();
@@ -20,14 +24,27 @@ public class VoyageTest {
 	@Test
 	public void should_have_two_town_not_equals() {
 		// GIVEN
-		Town given_1 = new Town();
-		Town given_2 = new Town();
+		Town given_1 = new Town(PARIS);
+		Town given_2 = new Town(LYON);
 		
 		// WHEN
 		boolean actual = given_1.getName().equals(given_2.getName());
 		
 		// THEN
 		assertThat(actual).isFalse();
+	}
+	
+	@Test
+	public void should_have_same_town_be_equals_itself() {
+		// GIVEN
+		Town given_1 = new Town(PARIS);
+		Town given_2 = new Town(PARIS);
+		
+		// WHEN
+		boolean actual = given_1.getName().equals(given_2.getName());
+		
+		// THEN
+		assertThat(actual).isTrue();
 	}
 	
 }
